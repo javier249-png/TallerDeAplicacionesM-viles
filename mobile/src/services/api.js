5d1fcd753407a1ed319a746154b5474f92a8d393
daylanwanderlust <dylsoto@cftla.cl>
2026-09-08 22:26:30 -0300

    feat: estructura inicial de carpetas src y cliente api en mobile

---
+++ mobile/src/services/api.js
@@ -0,0 +1,14 @@
+﻿const API_URL = process.env.EXPO_PUBLIC_API_URL || process.env.API_URL;
+
+export const fetchData = async (endpoint) => {
+  try {
+    const response = await fetch(`${API_URL}/${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`Error HTTP! estado: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error al conectar con el servidor:", error);
+    throw error;
+  }
+};
